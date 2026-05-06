80d913ef9703a651d8148fe365738b0c7033607d
YasminFerrari <161326008+Ferrari65@users.noreply.github.com>
2026-05-06 12:32:20 -0300

    fix: destaque temporada CRUD v5

--- src/main/java/com/Rpg/sistem_mayfair/repository/DestaqueTemporadaRepository.java
+++ src/main/java/com/Rpg/sistem_mayfair/repository/DestaqueTemporadaRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface DestaqueTemporadaRepository
-        extends JpaRepository<DestaqueTemporada, Long> {
+        extends JpaRepository<DestaqueTemporada, String> {
 
 
     Optional<DestaqueTemporada>
