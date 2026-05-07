c32866a74e7f028b37a94ad0e40e6f25c36a146f
YasminFerrari <161326008+Ferrari65@users.noreply.github.com>
2026-05-07 12:19:51 -0300

    fix: date

--- src/main/java/com/Rpg/sistem_mayfair/domain/HistoricoPrestigio.java
+++ src/main/java/com/Rpg/sistem_mayfair/domain/HistoricoPrestigio.java
@@ -1,5 +1,6 @@
 package com.Rpg.sistem_mayfair.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.*;
 
--- src/main/java/com/Rpg/sistem_mayfair/dto/HistoricoPrestigioDTO.java
+++ src/main/java/com/Rpg/sistem_mayfair/dto/HistoricoPrestigioDTO.java
@@ -1,8 +1,11 @@
 package com.Rpg.sistem_mayfair.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
 
 @Data
 @AllArgsConstructor
@@ -11,4 +14,6 @@ public class HistoricoPrestigioDTO {
     private Long idPersonagem;
     private String descricao;
     private Integer pontos;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+    private OffsetDateTime createdAt;
 }
