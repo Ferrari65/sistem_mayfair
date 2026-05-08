1d2a6ace300d1b8cd6b5c4aa7a02da814daf61cb
YasminFerrari <161326008+Ferrari65@users.noreply.github.com>
2026-05-08 11:52:00 -0300

    fix: date v9

--- src/main/java/com/Rpg/sistem_mayfair/domain/estabelecimento/FotoEstabelecimento.java
+++ src/main/java/com/Rpg/sistem_mayfair/domain/estabelecimento/FotoEstabelecimento.java
@@ -1,5 +1,6 @@
 package com.Rpg.sistem_mayfair.domain.estabelecimento;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -21,6 +22,7 @@ public class FotoEstabelecimento {
      */
     private String imageUrl;
 
+    @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "estabelecimento_id")
     private Estabelecimento estabelecimento;
