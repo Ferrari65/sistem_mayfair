a830199f795fd089cdcd775b14645d1cbd671346
YasminFerrari <161326008+Ferrari65@users.noreply.github.com>
2026-05-13 09:45:21 -0300

    fix: date v19

--- src/main/java/com/Rpg/sistem_mayfair/dto/personagem/PersonagemResponseDTO.java
+++ src/main/java/com/Rpg/sistem_mayfair/dto/personagem/PersonagemResponseDTO.java
@@ -1,11 +1,13 @@
 package com.Rpg.sistem_mayfair.dto.personagem;
 
+import com.Rpg.sistem_mayfair.domain.Enum.StatusCivil;
 import com.Rpg.sistem_mayfair.domain.Personagem;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public record PersonagemResponseDTO(
+
         String id,
         String name,
         String family,
@@ -14,18 +16,23 @@ public record PersonagemResponseDTO(
         String description,
         String imageUrl,
         List<Object> events,
-        String shape
+        String shape,
+
+        StatusCivil statusCivil
+
 ) {
 
-    // ✔ DEFAULT (SEM SHAPE)
     public PersonagemResponseDTO(Personagem personagem) {
         this(personagem, false);
     }
 
-    // ✔ COM CONTROLE DE ADMIN
-    public PersonagemResponseDTO(Personagem personagem, boolean isAdmin) {
+    public PersonagemResponseDTO(
+            Personagem personagem,
+            boolean isAdmin
+    ) {
         this(
                 String.valueOf(personagem.getId_personagens()),
+
                 personagem.getNome(),
 
                 personagem.getFamilia() != null
@@ -33,12 +40,20 @@ public record PersonagemResponseDTO(
                         : "Sem Família",
 
                 personagem.getTitulo(),
+
                 personagem.getPrestigio(),
+
                 personagem.getDescricao(),
+
                 personagem.getImageUrl(),
+
                 new ArrayList<>(),
 
-                isAdmin ? personagem.getShape() : null
+                isAdmin
+                        ? personagem.getShape()
+                        : null,
+
+                personagem.getStatusCivil()
         );
     }
 }
